0bd390c04ea61ada8497313013ca54246265c824
Berekto <bereketalebachew87@gmail.com>
2023-06-06 14:41:14 +0300

    added a method to return email & password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,3 +35,15 @@ class BasicAuth(Auth):
         except Exception as e:
             decoded_string = None
         return decoded_string
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """returns the user email and password
+            from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None or \
+                not isinstance(decoded_base64_authorization_header, str) or \
+                ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        email, password = decoded_base64_authorization_header.split(':')
+        return (email, password)
